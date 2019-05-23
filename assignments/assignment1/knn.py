0d81ed7950e874e1e3cc858bcf76fc1b974e818c
Mykola Kolombet <kolya.kolombet@gmail.com>
2019-05-23 15:42:08 -0400

    binary knn

--- assignments/assignment1/knn.py
+++ assignments/assignment1/knn.py
@@ -1,10 +1,12 @@
 import numpy as np
+from scipy.spatial.distance import cdist
 
 
 class KNN:
     """
     K-neariest-neighbor classifier using L1 loss
     """
+
     def __init__(self, k=1):
         self.k = k
 
@@ -13,9 +15,9 @@ class KNN:
         self.train_y = y
 
     def predict(self, X, num_loops=0):
-        '''
+        """
         Uses the KNN model to predict clases for the data samples provided
-        
+
         Arguments:
         X, np array (num_samples, num_features) - samples to run
            through the model
@@ -24,7 +26,7 @@ class KNN:
         Returns:
         predictions, np array of ints (num_samples) - predicted class
            for each sample
-        '''
+        """
         if num_loops == 0:
             dists = self.compute_distances_no_loops(X)
         elif num_loops == 1:
@@ -38,96 +40,101 @@ class KNN:
             return self.predict_labels_multiclass(dists)
 
     def compute_distances_two_loops(self, X):
-        '''
+        """
         Computes L1 distance from every sample of X to every training sample
         Uses simplest implementation with 2 Python loops
 
         Arguments:
         X, np array (num_test_samples, num_features) - samples to run
-        
+
         Returns:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
-        '''
+        """
         num_train = self.train_X.shape[0]
         num_test = X.shape[0]
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
             for i_train in range(num_train):
                 # TODO: Fill dists[i_test][i_train]
-                pass
+                dists[i_test][i_train] = np.sum(np.abs(np.diff([self.train_X[i_train], X[i_test]], axis=0)))
+        return dists
 
     def compute_distances_one_loop(self, X):
-        '''
+        """
         Computes L1 distance from every sample of X to every training sample
-        Vectorizes some of the calculations, so only 1 loop is used
+        Vectorized some of the calculations, so only 1 loop is used
 
         Arguments:
         X, np array (num_test_samples, num_features) - samples to run
-        
+
         Returns:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
-        '''
+        """
         num_train = self.train_X.shape[0]
         num_test = X.shape[0]
         dists = np.zeros((num_test, num_train), np.float32)
         for i_test in range(num_test):
             # TODO: Fill the whole row of dists[i_test]
             # without additional loops or list comprehensions
-            pass
+            dists[i_test] = np.sum(np.abs(self.train_X - X[i_test]), axis=1)
+        return dists
 
     def compute_distances_no_loops(self, X):
-        '''
+        """
         Computes L1 distance from every sample of X to every training sample
         Fully vectorizes the calculations using numpy
 
         Arguments:
         X, np array (num_test_samples, num_features) - samples to run
-        
+
         Returns:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
-        '''
+        """
         num_train = self.train_X.shape[0]
         num_test = X.shape[0]
         # Using float32 to to save memory - the default is float64
-        dists = np.zeros((num_test, num_train), np.float32)
         # TODO: Implement computing all distances with no loops!
-        pass
+        return cdist(X, self.train_X, 'cityblock')
 
     def predict_labels_binary(self, dists):
-        '''
+        """
         Returns model predictions for binary classification case
-        
+
         Arguments:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
 
         Returns:
-        pred, np array of bool (num_test_samples) - binary predictions 
+        pred, np array of bool (num_test_samples) - binary predictions
            for every test sample
-        '''
+
+        """
+
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.bool)
         for i in range(num_test):
-            # TODO: Implement choosing best class based on k
-            # nearest training samples
-            pass
+            prediction = 0
+            for j in range(self.k):
+                sortedlist = np.sort(dists[i])
+                prediction = prediction + 1 if self.train_y[np.where(dists[i] == sortedlist[j])] == 1 else prediction
+            pred[i] = 1 if prediction/self.k >= 0.5 else 0
         return pred
 
     def predict_labels_multiclass(self, dists):
-        '''
+        """
         Returns model predictions for multi-class classification case
-        
+
         Arguments:
         dists, np array (num_test_samples, num_train_samples) - array
            with distances between each test and each train sample
 
         Returns:
-        pred, np array of int (num_test_samples) - predicted class index 
+        pred, np array of int (num_test_samples) - predicted class index
            for every test sample
-        '''
+        """
         num_test = dists.shape[0]
         num_test = dists.shape[0]
         pred = np.zeros(num_test, np.int)
--- assignments/assignment1/metrics.py
+++ assignments/assignment1/metrics.py
@@ -18,7 +18,21 @@ def binary_classification_metrics(prediction, ground_truth):
     # Some helpful links:
     # https://en.wikipedia.org/wiki/Precision_and_recall
     # https://en.wikipedia.org/wiki/F1_score
-    
+    rightClassified = 0
+    truePositives = 0
+    falsePositives = 0
+    falseNegatives = 0
+    for i in range(prediction.shape[0]):
+        rightClassified = rightClassified + 1 if prediction[i] == ground_truth[i] else rightClassified
+        truePositives = truePositives + 1 if (prediction[i] == ground_truth[i] and prediction[i] == True) else truePositives
+        falsePositives = falsePositives + 1 if (prediction[i] == True and ground_truth[i] == False) else falsePositives
+        falseNegatives = falseNegatives + 1 if (prediction[i] == False and ground_truth[i] == True) else falseNegatives
+
+    accuracy = rightClassified * 100 / prediction.shape[0]
+    recall = truePositives * 100 / (truePositives + falseNegatives)
+    precision = truePositives * 100 / (truePositives + falsePositives)
+    f1 = 2 * precision * recall / (precision + recall)
+
     return precision, recall, f1, accuracy
 
 
